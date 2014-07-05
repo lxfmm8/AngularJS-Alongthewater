41f422e787a81c1a08e4f37a67e21ce3b601d0bc
Timmi Lin <linxiaofeng007@yahoo.com>
2014-07-05 02:06:45 -0400

    initial commit

---
+++ js/controllers.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl3', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl4', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl5', ['$scope', function($scope) {
+
+  }]);
